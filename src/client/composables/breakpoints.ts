99d8f6d1a078a1f956ef2dba458f14fa4c6e4be5
Your Name <you@example.com>
2025-06-02 19:36:28 +0700

    Update

--- src/client/composables/breakpoints.ts
+++ src/client/composables/breakpoints.ts
@@ -1,14 +1,20 @@
-import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
+// import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
 
 
 export const useBreakpointsGlobal = () => {
-  const breakpoints = useBreakpoints(breakpointsTailwind, { ssrWidth: 430 })
-  
-  const isSM = breakpoints.greaterOrEqual('sm')
-  const isMD = breakpoints.greaterOrEqual('md')
-  const isLG = breakpoints.greaterOrEqual('lg')
-  const isXL = breakpoints.greaterOrEqual('xl')
-  const is2XL = breakpoints.greaterOrEqual('2xl')
+  // const breakpoints = useBreakpoints(breakpointsTailwind, { ssrWidth: 430 })
+
+  // const isSM = breakpoints.greaterOrEqual('sm')
+  // const isMD = breakpoints.greaterOrEqual('md')
+  // const isLG = breakpoints.greaterOrEqual('lg')
+  // const isXL = breakpoints.greaterOrEqual('xl')
+  // const is2XL = breakpoints.greaterOrEqual('2xl')
+
+  const isSM = true
+  const isMD = true
+  const isLG = true
+  const isXL = true
+  const is2XL = true
 
   return { isSM, isMD, isLG, isXL, is2XL }
 }
